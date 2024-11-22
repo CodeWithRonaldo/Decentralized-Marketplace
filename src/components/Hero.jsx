b84c58ac851e2784e582672e31beb81ddef3c942
Abdulkudus777 <Abdulkudusakanji777@gmail.com>
2024-11-22 15:47:30 +0000

    Add Hero, buttons and update button colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,19 @@ const Hero = () => {
   return (
     <>
       <div className="hero">
-        <h1>Market</h1>
-
-        <button className="buy-btn">Buy</button>
+        <div className="hero-text">
+          <h1>Your Gateway to a Decentralized Marketplace</h1>
+          <h2>Discover a Decentralized Marketplace Built on Trust</h2>
+          <p>
+            Explore a marketplace designed to give you full control. Sell
+            products globally, make secure crypto payments, and access a
+            transparent transaction history—all in one place
+          </p>
+        </div>
+        <div className="hero-btn">
+          <button className="started-btn">Get Started</button>
+          <button className="buy-btn">Buy</button>
+        </div>
       </div>
     </>
   );
